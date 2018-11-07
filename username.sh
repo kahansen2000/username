be863c3f775bccebf1330906cf83b57435e0b137
Katherine Hansen <13894347+kahansen2000@users.noreply.github.com>
2018-11-06 22:22:15 -0500

    Update username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 echo "Enter a username: "
 echo "The username must contain 3-12 characters, begin with a letter, and only contain lowercase letters, numbers, and _ characters"
 read username
-while echo $username | egrep -v "^[a-z][a-z0-9_]{3-12}$" > /dev/null 2>&1
+while echo $username | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "Enter a valid username: "
 	echo "The username must contain 3-12 characters, begin with a letter, and only contain lowercase letters, numbers, and _ characters"
